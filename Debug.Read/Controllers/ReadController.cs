67ecfae006aef7c591588e0bcade55d3da4cce5d
AndreeNoA <55133543+AndreeNoA@users.noreply.github.com>
2021-01-08 13:41:20 +0100

    Remade parts and added docker

---
+++ Debug.Read/Controllers/ReadController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReadApi.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Debug.Read.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReadController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "hejsan";
+        }
+    }
+}
---
+++ FrontEnd/Pages/Privacy.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            ViewData["Message"] = "Hello from webfrontend";
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                // Call *mywebapi*, and display its response in the page
+                var request = new System.Net.Http.HttpRequestMessage();
+                request.RequestUri = new Uri("http://debug.read/Read"); // ASP.NET 3 (VS 2019 only)
+                //request.RequestUri = new Uri("http://mywebapi/api/values/1"); // ASP.NET 2.x
+                var response = await client.SendAsync(request);
+                ViewData["Message"] += " and " + await response.Content.ReadAsStringAsync();
+            }
+        }
+
+    }
+}
